865c6fcefc73abcc9b0ef5c96fb414192d44719d
MatheusAugusto19 <matheusaugu327@gmail.com>
2025-11-30 19:49:30 -0300

    refactor: simplify provider nesting with StoreProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.scss'
-import { ProductProvider } from './context/ProductProvider'
-import { CartProvider } from './context/CartContext'
-import { NotificationProvider } from './context/NotificationContext'
-import { OrderProvider } from './context/OrderContext'
-import { WishlistProvider } from './context/WishlistContext'
-import { ReviewProvider } from './context/ReviewContext'
-import { CouponProvider } from './context/CouponContext'
-import { FreightProvider } from './context/FreightContext'
-import { AuthProvider } from './context/AuthContext'
+import StoreProvider from './context/StoreProvider'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NotificationContainer from './components/NotificationContainer'
@@ -20,38 +12,22 @@ import WishlistPage from './pages/WishlistPage'
 
 function App() {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <WishlistProvider>
-          <OrderProvider>
-            <ProductProvider>
-              <ReviewProvider>
-                <CouponProvider>
-                  <FreightProvider>
-                    <NotificationProvider>
-                      <div className="app-layout">
-                        <Header />
-                        <main className="app-main">
-                          <NotificationContainer />
-                          <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/cart" element={<CartPage />} />
-                            <Route path="/checkout" element={<CheckoutPage />} />
-                            <Route path="/orders" element={<OrderHistoryPage />} />
-                            <Route path="/wishlist" element={<WishlistPage />} />
-                          </Routes>
-                        </main>
-                        <Footer />
-                      </div>
-                    </NotificationProvider>
-                  </FreightProvider>
-                </CouponProvider>
-              </ReviewProvider>
-            </ProductProvider>
-          </OrderProvider>
-        </WishlistProvider>
-      </CartProvider>
-    </AuthProvider>
+    <StoreProvider>
+      <div className="app-layout">
+        <Header />
+        <main className="app-main">
+          <NotificationContainer />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/wishlist" element={<WishlistPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </StoreProvider>
   )
 }
 
